e86e44e61e43f678e27fb69cd95983ccca88b625
misteregis <misteregis@gmail.com>
2023-08-06 20:03:04 -0300

    Criado migrações

---
+++ Migrations/20230806230034_InitialCreate.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DioTrilhaNetBancoDeDadosDesafio.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "actors",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    first_name = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true),
+                    last_name = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true),
+                    gender = table.Column<string>(type: "TEXT", maxLength: 1, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_actors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "genres",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    gender = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genres", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "movie_genres",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    gender_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    movie_id = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movie_genres", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    year = table.Column<int>(type: "INTEGER", nullable: false),
+                    duration = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movies", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "movies_cast",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    actor_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    movie_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    role = table.Column<string>(type: "TEXT", maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_movies_cast", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "actors");
+
+            migrationBuilder.DropTable(
+                name: "genres");
+
+            migrationBuilder.DropTable(
+                name: "movie_genres");
+
+            migrationBuilder.DropTable(
+                name: "movies");
+
+            migrationBuilder.DropTable(
+                name: "movies_cast");
+        }
+    }
+}
